3a95cb18f4d5de63a784d40240af193607803617
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-06 13:07:16 -0400

    exercise 1

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 11;
+            int b = 3;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+        }
+    }
+
+}
