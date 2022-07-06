cad3192c4c18d9606c11d67819324b2bc9f82579
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-06 14:41:35 -0400

    added AreaOfCircle method

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,12 +6,25 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
-            int a = 11;
-            int b = 3;
+            // Exercise 1:
+            int a = 17;
+            int b = 4;
             int quotient = a / b;
             int remainder = a % b;
 
             Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            // Exercise 2:
+            Console.WriteLine("Enter radius of circle: ");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with radius {radius} is {AreaOfCircle(radius)}");
+        }
+
+        //Create a method that will Calculate the area of a circle based on its radius
+        public static double AreaOfCircle(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
         }
     }
 
