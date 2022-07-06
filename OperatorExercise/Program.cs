2f6d4ec12d5d255f23bbe65a3f792cc0a078e04a
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-06 16:42:41 -0400

    added missing operators and if-else statement to exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -9,10 +9,26 @@ namespace OperatorExercise
             // Exercise 1:
             int a = 17;
             int b = 4;
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
             int quotient = a / b;
             int remainder = a % b;
 
-            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a} + {b} is {sum}");
+                Console.WriteLine($"{a} - {b} is {difference}");
+                Console.WriteLine($"{a} x {b} is {product}");
+                Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            }
+            else
+            {
+                Console.WriteLine($"The sum of these two number is probably not {sum}");
+                Console.WriteLine($"The difference of these two numbers is probably not {difference}");
+                Console.WriteLine($"The product of these two numbers is probably not {product}");
+                Console.WriteLine($"The quotient of these two numbers is probably not {quotient} remainder {remainder}");
+            }
 
             // Exercise 2:
             Console.WriteLine("Enter radius of circle: ");
